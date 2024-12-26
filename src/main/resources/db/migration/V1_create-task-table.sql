8f2d0e7c249c5a12c7a43dc93cdfa1d25584b5cc
vitor-gm <gm.0509rbt@gmail.com>
2024-12-26 11:47:03 -0300

    update `db.migration`

--- src/main/resources/db/migration/V1_create-task-table.sql
+++ src/main/resources/db/migration/V1_create-task-table.sql
@@ -1,4 +1,6 @@
-CREATE TABLE teamTasks (
+CREATE DATABASE team_tasks;
+USE team_tasks;
+CREATE TABLE Tasks (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT,
