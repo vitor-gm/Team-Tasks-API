432de53d9642f5503911aceaa50e7120d04c87b2
vitor-gm <gm.0509rbt@gmail.com>
2024-12-28 06:13:36 -0300

    add `AuthenticationDTO` file

---
+++ src/main/java/com/example/team_tasks/controller/AuthenticationController.java
@@ -0,0 +1,31 @@
+package com.example.team_tasks.controller;
+
+import com.example.team_tasks.model.user.AuthenticationDTO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+public class AuthenticationController {
+
+    private final AuthenticationManager authenticationManager;
+    public AuthenticationController(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.ok().build();
+
+    }
+}
